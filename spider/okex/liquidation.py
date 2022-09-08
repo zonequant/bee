1e08e89aa859ef17b94d6cf211e7721f727bbd7d
dominolu <dominolu@163.com>
2022-09-08 21:20:39 +0800

    调试时间重复

--- spider/okex/liquidation.py
+++ spider/okex/liquidation.py
@@ -51,8 +51,10 @@ class Okex(Spider):
         url = "https://www.okx.com/api/v5/public/liquidation-orders"
         param = {"uly": symbol, "instType": "SWAP", "state": "filled", "before": ts}
         next_ts=get_timestamp_ms()+self.period*1000
-        self.request_delay(next_ts,url=url, data=param,method="GET", callback="parse_liquidation")
-        log.info(f"推入延时队列{next_ts}-{param}")
+        if symbol=="BTC-USDT":
+            self.request_delay(next_ts,url=url, data=param,method="GET", callback="parse_liquidation")
+            log.info(f"推入延时队列{next_ts}-{param}")
+
     async def pipe_item(self, items):
         # todo 保存数据库
         await self.db.process_item("liquidation",items)
@@ -72,7 +74,7 @@ class Okex(Spider):
             # await self.request(url, data=param, callback="parse_liquidation")
             for i in data:
                 symbol=i["uly"]
-                if i["ctValCcy"]!="USD" and symbol=="BTC-USDT":
+                if i["ctValCcy"]!="USD":
                     param = await self.get_param(symbol)
                     ctVal=i["ctVal"]
                     self.markets[symbol]=float(ctVal)
